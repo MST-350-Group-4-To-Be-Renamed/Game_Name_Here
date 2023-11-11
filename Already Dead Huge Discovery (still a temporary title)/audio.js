8cc7a02ed8c010b7b94d9e83cfa54be7d07ffea3
Dan4C0D3 <104708405+Dan4C0D3@users.noreply.github.com>
2023-11-11 11:03:20 -0700

    Fade out effect

--- Already Dead Huge Discovery (still a temporary title)/audio.js
+++ Already Dead Huge Discovery (still a temporary title)/audio.js
@@ -2,3 +2,25 @@ function set_audio_speed(id, speed) {
     var audio = document.getElementById(id);
     audio.playbackRate = speed;
 }
+
+function fade_out (id, time, new_audio) {
+	var audio = document.getElementById(id);
+	var step = time * 10;
+	
+	var interval = setInterval(function() {set_volume(audio)}, step);
+	setTimeout(function() {clear_interval(interval, audio, new_audio, id)}, ((time*1000) + 8));// id, new_audio)}, 1020);
+}
+
+function set_volume (audio) {
+	audio.volume -= 0.01;
+}
+
+function clear_interval (interval, audio, new_audio, id) {
+	clearInterval(interval);
+	audio.volume = 0;
+	ASLEvent('DestroyAudio', id);
+	if (new_audio != null) {
+		ASLEvent('PlayAudio', new_audio);
+	}
+	
+}
